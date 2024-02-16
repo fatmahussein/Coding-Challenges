8e2bcebb67ecbdc19054daa71ad7463ff559114d
Fatma Hussein <fartunhussein346@gmail.com>
2024-02-16 23:58:05 +0300

    sort arr in ascending order

---
+++ bubbleSort.js
@@ -0,0 +1,21 @@
+// sorting array in ascending order
+const array = [3,1,2,6,7,4]
+
+function bubbleSort(array) {
+    // Only change code below this line
+    for (let i = 0; i < array.length; i++) {
+
+      for (let j = 0; j < array.length - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          const temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+      }
+
+    }
+    return array;
+    // Only change code above this line
+  }
+
+  console.log(bubbleSort(array));
