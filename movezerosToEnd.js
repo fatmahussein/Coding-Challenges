a282e76361310e8ebdd593857572a17b78177f0e
Fatma Hussein <fartunhussein346@gmail.com>
2024-02-16 23:58:22 +0300

    move zeroes to the end

---
+++ movezerosToEnd.js
@@ -0,0 +1,20 @@
+// Given an integer array nums, move all 0's to the end of it while 
+//maintaining the relative order of the non-zero elements.
+
+function moveZerosToEnd(arr) {
+    let ZeroIndex = 0;
+ 
+     for (let i = 0; i < arr.length; i++) {
+         // console.log(arr[i]);
+         if(arr[i] !== 0) {
+            [arr[i], arr[ZeroIndex]] = [arr[ZeroIndex], arr[i]];
+             ZeroIndex++;
+         }
+        //  console.log(arr[ZeroIndex]);
+ 
+     }
+     return arr;
+ } 
+ 
+ let  arr = [1, 10, 0, 2, 8, 3, 0, 0, 6, 4, 0, 5, 7, 0]
+ console.log(moveZerosToEnd(arr));
