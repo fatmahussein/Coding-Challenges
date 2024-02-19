931f6fd6717e8bd23399e2aacfb9adf4a1427a57
Fatma Hussein <fartunhussein346@gmail.com>
2024-02-19 13:49:34 +0300

    Difference of two arrays

--- DiffArr.js
+++ DiffArr.js
@@ -4,17 +4,15 @@ function DiffArray(arr1,arr2){
     for(let i=0; i < arr1.length; i += 1){
         if(arr2.indexOf(arr1[i]) === -1){
             newArr.push(arr1[i]);
-    }
-}
+         }
+        }
 
-for(let j=0; j < arr2.length; j += 1){
-    if(arr1.indexOf(arr2[j]) === -1){
-        newArr.push(arr2[j]);
-}
-}
-
-return newArr;
-
+    for(let j=0; j < arr2.length; j += 1){
+        if(arr1.indexOf(arr2[j]) === -1){
+                newArr.push(arr2[j]);
+            }
+        }
+    return newArr;
 }
 console.log(DiffArray(
     [1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5]
